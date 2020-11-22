537c59fa26245b3c7bf595c70856db4237735e38
Panasavv <panasavv3@gmail.com>
2020-11-22 15:59:08 +0200

    1st Assignment

---
+++ Panasavv/ProgramInC/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char c[26];
+    for(int i =0 ; i<26; i++){
+        c[i]=65+i;
+    printf("%c\n", c[i]);
+        }
+    return 0;
+}
