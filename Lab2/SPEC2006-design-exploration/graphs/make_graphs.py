d5f02a2cc11d84e30cad57f5b1a2343bd25f4e37
Grigoris Pavlakis <grigpavl@ece.auth.gr>
2020-12-10 23:46:26 +0200

    Create graphs of all parameters

--- Lab2/SPEC2006-design-exploration/graphs/make_graphs.py
+++ Lab2/SPEC2006-design-exploration/graphs/make_graphs.py
@@ -4,9 +4,9 @@ import numpy as np
 from sys import argv
 
 CPI_INDEX = 0
-DCACHE_MISSRATE = 1
-ICACHE_MISSRATE = 2
-L2_MISSRATE = 3
+DCACHE_MISSRATE_INDEX = 1
+ICACHE_MISSRATE_INDEX = 2
+L2_MISSRATE_INDEX = 3
 
 def read_file(file):
     with open(file) as f:
@@ -21,12 +21,12 @@ def read_file(file):
     return parsed_data
 
 
-def create_plot(title, x_label, data_dict, data_point_index, boundary_line, lower_xlim, upper_xlim, file_name):
+def create_plot(title, x_label, data_dict, data_point_index, boundary_line, file_name):
 
     labels = ["64k L1i size", "4-way L1i assoc.", "8-way L1i assoc.", "32k L1d size", \
     "128k L1d size", "4-way L1d assoc.", "8-way L1d assoc.", "1MB L2 size", "4MB L2 size", \
     "2-way L2 assoc.", "4-way L2 assoc.", "32b line size", "128b line size", "256b line size",
-    "256k L1d size"]
+    "256k L1d size", "Optimal CPI case"]
     
     figure, axes = plt.subplots()
     axes.axvline(boundary_line, color='r')
@@ -35,7 +35,6 @@ def create_plot(title, x_label, data_dict, data_point_index, boundary_line, lowe
     axes.spines["right"].set_visible(False)
     axes.spines["top"].set_visible(False)
     
-    axes.set_xlim(lower_xlim, upper_xlim)
     axes.set_xlabel(x_label)
     results = []
     for key in list(data_dict.keys())[1:]:
@@ -47,17 +46,23 @@ def create_plot(title, x_label, data_dict, data_point_index, boundary_line, lowe
     axes.set_yticks(bar_positions)
     axes.set_yticklabels(labels)
 
+    bound = (max(results) - min(results)) * 0.05
+    clamped_min_x = max(0, min(results) - bound)
+    axes.set_xlim(clamped_min_x, max(results) + bound)
+
     for rect in rects:
         length = rect.get_width()
         axes.annotate('{}'.format(length),
-                    xy=(length + 0.002, rect.get_y() + (rect.get_height() / 2)),
+                    xy=(length, rect.get_y() + (rect.get_height() / 2)),
                     va='center')
 
     plt.savefig(file_name, bbox_inches='tight')
     axes.clear()
 
 if __name__ == "__main__":
-    t = read_file(argv[1])
-    create_plot("lol", "CPI (lower is better)", t, CPI_INDEX, t["spechmmer"][CPI_INDEX], 1, 1.4, "test1.png")
-    create_plot("lol", "CPI (lower is better)", t, CPI_INDEX, t["spechmmer"][CPI_INDEX], 1, 1.4, "test2.png")
+    data = read_file(argv[1])
+    create_plot(argv[2].capitalize() + " CPI", "CPI (lower is better)", data, CPI_INDEX, data[argv[2]][CPI_INDEX], argv[2] + "-cpi.png")
+    create_plot(argv[2].capitalize() + " L1 data cache overall miss rate", "Miss rate (lower is better)",data, DCACHE_MISSRATE_INDEX, data[argv[2]][DCACHE_MISSRATE_INDEX], argv[2] + "-dcmiss.png")
+    create_plot(argv[2].capitalize() + " L1 instr. cache overall miss rate", "Miss rate (lower is better)", data, ICACHE_MISSRATE_INDEX, data[argv[2]][ICACHE_MISSRATE_INDEX], argv[2] + "-icmiss.png")
+    create_plot(argv[2].capitalize() + " L2 cache overall miss rate", "Miss rate (lower is better)", data, L2_MISSRATE_INDEX, data[argv[2]][L2_MISSRATE_INDEX], argv[2] + "-l2miss.png")
     
