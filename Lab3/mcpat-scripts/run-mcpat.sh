730aa9c9a88dec0437567cc47b324379c6e4704f
Grigoris Pavlakis <grigpavl@ece.auth.gr>
2020-12-16 19:20:31 +0200

    Add helper scripts for McPAT data processing

---
+++ Lab3/mcpat-scripts/run-mcpat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for INPUT in $(find . -name spec*.xml | head -1)
+do
+	echo $INPUT
+	DEST_DIR=mcpat-results/$(basename $(dirname $INPUT))
+	mkdir -p $DEST_DIR
+	$1/mcpat -infile $INPUT -print_level 5 > $DEST_DIR/$(basename $INPUT).txt
+done
